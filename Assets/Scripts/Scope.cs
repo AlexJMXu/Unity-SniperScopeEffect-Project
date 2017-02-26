9416ecf32da1d8c61ee80883f04cd9f634c908b8
Alexander Xu <ajm.xu@outlook.com>
2017-02-26 18:59:59 +0000

    Initial Commit

---
+++ Assets/Scripts/Scope.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scope : MonoBehaviour {
+
+	public Animator animator;
+	public GameObject scopeOverlay;
+	public GameObject weaponCamera;
+	public Camera mainCamera;
+
+	private bool isScoped = false;
+
+	public float scopedFOV = 15f;
+	private float normalFOV;
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire2")) {
+			isScoped = !isScoped;
+			animator.SetBool("isScoped", isScoped);
+			if (isScoped) {
+				StartCoroutine(OnScoped());
+			} else {
+				OnUnscoped();
+			}
+		}
+	}
+
+	void OnUnscoped() {
+		scopeOverlay.SetActive(false);
+		weaponCamera.SetActive(true);
+
+		mainCamera.fieldOfView = normalFOV;
+	}
+
+	IEnumerator OnScoped() {
+		yield return new WaitForSeconds(0.15f);
+
+		scopeOverlay.SetActive(true);
+		weaponCamera.SetActive(false);
+
+		normalFOV = mainCamera.fieldOfView;
+		mainCamera.fieldOfView = scopedFOV;
+	}
+}
